b3acb212ec055c88455f04ca9f9f1eb782ff1a01
Arn Elacion - ImaginativePen <elacionarn@gmail.com>
2021-01-19 16:05:54 +0800

    Add files via upload

---
+++ vb-fetchDriveSerial/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Management
+Imports System.Management.Instrumentation
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show(getHDDID("E"))
+        MessageBox.Show(getHDDID("F"))
+        MessageBox.Show(getHDDID("G"))
+    End Sub
+
+    Public Shared Function getHDDID(ByVal drive As String) As String
+        Dim disk As ManagementObject = New ManagementObject("Win32_LogicalDisk.DeviceID=""" & drive & ":""")
+        disk.[Get]()
+        drive = disk("VolumeSerialNumber").ToString()
+        Return drive
+    End Function
+
+End Class
